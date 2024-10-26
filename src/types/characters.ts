57302fab67de6c0276558e47e944c39aedc9b1f7
Eliezer Capuano <capuanojahday0@gmail.com>
2024-10-25 20:42:06 -0400

    FIRST COMMIT

---
+++ src/types/characters.ts
@@ -0,0 +1,16 @@
+export interface Character {
+    id: number;
+    name: string;
+    ki: string;
+    maxKi: string;
+    race: string;
+    gender: string;
+    description: string;
+    image: string;
+    affiliation: string;
+    deletedAt: string | null;
+}
+
+export interface CharactersResponse {
+    items: Character[];
+}
